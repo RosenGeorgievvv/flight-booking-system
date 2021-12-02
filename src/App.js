915539b02eec6ff341823d348c8ffb6c145a7c5d
Rosen Georgiev <73111903+RosenGeorgievvv@users.noreply.github.com>
2021-12-02 11:05:31 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div>
+     <form>
+<input type="text" placeholder="First Name" />
+<input type="text" placeholder="Last Name" />
+<input type="text" placeholder="Departure Airport" />
+<input type="text" placeholder="Destination Airport" />
+<input type="text" placeholder="First Name" />
+<input type="text" placeholder="First Name" />
+
+
+     </form>
+    </div>
+  );
+}
+
+export default App;
