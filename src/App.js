8d0ded2d8c1d3eb45e77b3f329493727605b07fb
Rosen Georgiev <73111903+RosenGeorgievvv@users.noreply.github.com>
2021-12-04 10:30:58 +0200

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,96 +1,18 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState, useEffect } from 'react';
-import BookingForm from './components/BookingForm'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DetailsPage from './components/DetailsPage/DetailsPage';
+import LandingPage from './components/LandingPage/LandingPage';
 
-function App() {
-
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [departureAir, setDepartureAir] = useState(0);
-  const [arrivingAir, setArrivingAir] = useState(0);
-  const [departureDate, setDepartureDate] = useState('');
-  const [returnDate, setReturnDate] = useState('');
-  const [flightData, setFlightData] = useState([]);
-
-  console.log(firstName)
-  console.log(lastName)
-  console.log(departureAir)
-  console.log(arrivingAir)
-  console.log(departureDate)
-  console.log(returnDate)
-
-  const token = 'JU19y1QSmyW18urRqSfBoMYt3HH413'
-
-  const onUpdate = (data) => {
-    fetch(`https://vm-fe-interview-task.herokuapp.com/api/bookings?authToken=${token}&pageIndex=0`)
-      .then(response => response.json())
-      .then(data => setFlightData(data.list));
-  }
-
-
-  const onCreate = (data) => {
-
-    fetch(`https://vm-fe-interview-task.herokuapp.com/api/bookings/create?authToken=${token}`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data)
-    }).then(response => {
-      console.log(response)
-    })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-
-    if (firstName && lastName && departureAir && arrivingAir && departureDate && returnDate) {
-      const data = {
-        firstName: firstName,
-        lastName: lastName,
-        departureAirportId: departureAir,
-        arrivalAirportId: arrivingAir,
-        departureDate: departureDate,
-        returnDate: returnDate
-      }
-      onCreate(data);
-
-    } else {
-      alert('Please fill up all the fields!')
-    }
-  }
-
-  useEffect(() => {
-    onUpdate();
-  }, [])
-
-
-  return (
-    <div>
-      <div className="justify-content-center d-flex" >
-        <BookingForm
-          setFirstName={setFirstName}
-          setLastName={setLastName}
-          setDepartureAir={setDepartureAir}
-          setArrivingAir={setArrivingAir}
-          setDepartureDate={setDepartureDate}
-          setReturnDate={setReturnDate}
-          onSubmitHandler={onSubmitHandler}
-        />
-      </div>
-      <div className="justify-content-center d-flex mt-5">
-        <ul>
-          {flightData.map((flight, i) => (
-            <li key={i}>{flight.id}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route exact path='/' element={<LandingPage />} />
+                    <Route exact path='/details/:flightId' element={<DetailsPage />} />
+                </Routes>
+            </Router>
+        </div>
+    )
 }
 export default App;
